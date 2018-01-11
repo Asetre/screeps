64cb2e1bf7a8265560faa50c52fe17f25e4938bd
Asetr <asetre.paul@gmail.com>
2018-01-10 20:13:24 -0800

    updates

--- main.js
+++ main.js
@@ -140,11 +140,13 @@ Harvesters.forEach(function (creep, index) {
 });
 
 function harvestEnergy(creep, source) {
-    if (creep.carry.energy < creep.carryCapacity) {
+    if (creep.memory.job === 'harvest') {
+        if (creep.carry.energy === creep.carryCapacity) creep.memory.job = 'transfer';
         if (creep.harvest(source) === ERR_NOT_IN_RANGE) {
             creep.moveTo(source);
         }
     } else {
+        if (creep.carry.energy === 0) creep.memory.job = 'harvest';
         if (creep.transfer(spawn1, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
             creep.moveTo(spawn1);
         }
